4b4825f80d073a836f1858a64302d3d9654726f1
hi <kangja1996@gmail.com>
2020-10-12 16:22:05 -0400

    progress

--- version2.js
+++ version2.js
@@ -1,34 +1,31 @@
-// Write a function, `anagrams(str1, str2)`, that takes in two words and returns 
-// a boolean indicating whether or not the words are anagrams. Anagrams are 
-// words that contain the same characters but not necessarily in the same order. 
-// Solve this WITHOUT using Array.prototype.sort.
-// 
-// Examples:
-// anagrams('listen', 'silent') => true
-// anagrams('listen', 'potato') => false
-
-
-// PSEUDOCODE
-// check if two words have the matching alphabets. if they do, return true. 
-
-// CODE
 function anagrams(str1, str2) {
-  let emptyArray = [];
+  let hash1 = {}
+
   for (let i = 0; i < str1.length; i++) {
-    for (let j = 0; j < str2.length; j++) {
-      if (str1[i] == str2[j]) {
-        emptyArray.push(str2[j])
-      }
+    if (hash1[str1[i]]) {
+      hash1[str1[i]] += 1
+    }
+    else {
+      hash1[str1[i]] = 1
     }
   }
-  // return emptyArray
-  if (emptyArray.join("") == str1) { //emptyArray.join("") = listen
-    return true
-  } else {
-    return false
+  
+  for (let i = 0; i < str2.length; i++) {
+    if (hash1[str2[i]]) {
+      hash1[str2[i]] -= 1
+    }
+    else {
+      hash1[str2[i]] = 1
+    }
   }
-}
 
+  for (const key in hash1) {
+    if (hash1[key] !== 0) {
+      return false
+    }
+  }
+  return true
+}
 
 console.log(anagrams("abc", "cba"));
 //   expect(anagrams('abc', 'cba')).toBe(true);
@@ -48,5 +45,3 @@ console.log(anagrams("aiuwehfxzxcvmneowieurahsde", "nsewceaerihfawzueouxdihmxv")
 console.log(anagrams("aiuwehfxzxcvmneowieurahsde", "nsewceaerihfawzueouxdihmxw"))
 //   expect(anagrams('aiuwehfxzxcvmneowieurahsde', 'nsewceaerihfawzueouxdihmxw')).toBe(false);
 // });
-
-
